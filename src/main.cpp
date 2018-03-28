dfd6eeefb1cd7b3c6a82adaeddf1bc22d0187cde
areinull <a.rei.null@gmail.com>
2018-03-28 22:37:46 +0300

    walk if no food; random splitting

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,26 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cstdlib>
 
 using namespace std;
 using json = nlohmann::json;
 
 struct Strategy {
+    enum class Quadrant {
+        LU,
+        RU,
+        LL,
+        RL
+    };
 
     void run() {
         string data;
         cin >> data;
         auto config = json::parse(data);
+
+        std::srand(time(nullptr));
+
         while (true) {
             cin >> data;
             auto parsed = json::parse(data);
@@ -27,15 +37,46 @@ struct Strategy {
             auto first = mine[0];
             auto food = find_food(objects);
             if (!food.empty()) {
-                return {{"X", food["X"]},
-                        {"Y", food["Y"]}};
+                return {{"X",     food["X"]},
+                        {"Y",     food["Y"]},
+                        {"Split", rand() % 100 > 95}};
+            } else {
+                const int cx = config["GAME_WIDTH"].get<int>() / 2;
+                const int cy = config["GAME_HEIGHT"].get<int>() / 2;
+                const float mx = first["X"].get<float>();
+                const float my = first["Y"].get<float>();
+
+                Quadrant q;
+                if (mx < cx && my < cy) q = Quadrant::LU;
+                else if (mx < cx && my >= cy) q = Quadrant::LL;
+                else if (mx >= cx && my < cy) q = Quadrant::RU;
+                else q = Quadrant::RL;
+
+                float x, y;
+                switch (q) {
+                    case Quadrant::LU:
+                        x = 0.f;
+                        y = config["GAME_HEIGHT"].get<int>();
+                        break;
+                    case Quadrant::LL:
+                        x = config["GAME_WIDTH"].get<int>();
+                        y = config["GAME_HEIGHT"].get<int>();
+                        break;
+                    case Quadrant::RL:
+                        x = config["GAME_WIDTH"].get<int>();
+                        y = 0.f;
+                        break;
+                    case Quadrant::RU:
+                        x = y = 0.f;
+                        break;
+                }
+                return {{"X",     x},
+                        {"Y",     y},
+                        {"Debug", "No food"}};
             }
-            return {{"X",     config["GAME_WIDTH"].get<int>()/2},
-                    {"Y",     config["GAME_HEIGHT"].get<int>()/2},
-                    {"Debug", "No food"}};
         }
-        return {{"X",     config["GAME_WIDTH"].get<int>()/2},
-                {"Y",     config["GAME_HEIGHT"].get<int>()/2},
+        return {{"X",     0},
+                {"Y",     0},
                 {"Debug", "Died"}};
     }
 
