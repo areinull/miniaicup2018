1480b12efda4d9b55477325f7f81248014f475e5
areinull <a.rei.null@gmail.com>
2018-04-06 21:14:19 +0300

    correct planned position 2

--- src/MovePlanner.cpp
+++ src/MovePlanner.cpp
@@ -37,7 +37,7 @@ V2d MovePlanner::plan(const nlohmann::json &mine, const V2d &dst) const {
     const float crossProd = vel.crossZ(dirDst);
     const V2d dirDstPerp{-dirDst.y, dirDst.x};
 
-    return pos + (dirDst + crossProd*dirDstPerp)*mine[maxMassIdx]["R"].get<float>()*1.2f;
+    return pos + (dirDst + crossProd*dirDstPerp)*mine[maxMassIdx]["R"].get<float>()*5.f;
 }
 
 bool MovePlanner::covered(const nlohmann::json &mine, const V2d &dst) const {
